921483c27b46cc75c8a2c74c1678cd46ed5b098a
jportella93 <jportella93@gmail.com>
2019-05-18 18:35:27 +0200

    feat: some changes on layout and style: indentation and spaces

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ class BlogIndex extends React.Component {
       <Layout location={this.props.location} title={siteTitle}>
         <SEO
           title="All posts"
-          keywords={[`blog`, `gatsby`, `javascript`, `react`]}
+          keywords={[ `blog`, `gatsby`, `javascript`, `react` ]}
         />
         {posts.map(({ node }) => {
           const title = node.frontmatter.title || node.fields.slug

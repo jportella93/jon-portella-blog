ac20717fa8a19b9a00615df5c0f8a35ec00613f4
jportella93 <jportella93@gmail.com>
2019-05-18 18:35:27 +0200

    feat: some changes on layout and style: indentation and spaces

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -5,6 +5,24 @@ import Image from 'gatsby-image'
 import { rhythm } from '../utils/typography'
 
 function Bio() {
+
+  const [ containerStyle, setContainerStyle ] = React.useState({
+    display: `flex`,
+    flexDirection: `column`,
+    alignItems: `center`,
+    marginBottom: rhythm(2.5),
+  })
+
+  React.useEffect(() => {
+    if (window.innerWidth > 450) {
+      setContainerStyle({
+        ...containerStyle,
+        flexDirection: `row`,
+        alignItems: `initial`
+      })
+    }
+  }, [])
+
   return (
     <StaticQuery
       query={bioQuery}
@@ -12,10 +30,7 @@ function Bio() {
         const { author, social } = data.site.siteMetadata
         return (
           <div
-            style={{
-              display: `flex`,
-              marginBottom: rhythm(2.5),
-            }}
+            style={containerStyle}
           >
             <Image
               fixed={data.avatar.childImageSharp.fixed}
@@ -27,21 +42,28 @@ function Bio() {
                 borderRadius: `100%`,
               }}
             />
-            <p>
-              Written by <strong>{author}</strong>.<br />
-              {` `}
-              <a href="http://bit.ly/jportella-twitter">
-                Twitter
-              </a>
-              {` `}
-              <a href="http://bit.ly/jportella-github">
-                GitHub
-              </a>
-              {` `}
-              <a href="http://bit.ly/jportella-linkedin">
-                LinkedIn
-              </a>
-            </p>
+            <div style={{
+              display: `flex`
+            }}>
+              <p style={{
+                margin: `auto 0`
+              }}>
+                Written by <strong>{author}</strong>.<br />
+                {` `}
+                <a href="http://bit.ly/jportella-twitter">
+                  Twitter
+                </a>
+                {` `}
+                <a href="http://bit.ly/jportella-github">
+                  GitHub
+                </a>
+                {` `}
+                <a href="http://bit.ly/jportella-linkedin">
+                  LinkedIn
+                </a>
+              </p>
+
+            </div>
           </div>
         )
       }}
@@ -53,7 +75,7 @@ const bioQuery = graphql`
   query BioQuery {
     avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
       childImageSharp {
-        fixed(width: 50, height: 50) {
+        fixed(width: 200, height: 200, quality: 100) {
           ...GatsbyImageSharpFixed
         }
       }

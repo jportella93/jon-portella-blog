8696f1d9281de6011d1662e90f7d0746bdb4cd6f
jportella93 <jportella93@gmail.com>
2019-06-01 13:45:38 +0200

    feat: increase max-width of layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -13,14 +13,14 @@ class Layout extends React.Component {
         style={{
           marginLeft: `auto`,
           marginRight: `auto`,
-          maxWidth: rhythm(24),
+          maxWidth: rhythm(32),
           padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
         }}
       >
-        <Header location={location} rootPath={rootPath} text={'<>'}/>
+        <Header location={location} rootPath={rootPath} text={'<>'} />
         {children}
         <footer>
-          <Header location={location} rootPath={rootPath} text={'</>'}/>
+          <Header location={location} rootPath={rootPath} text={'</>'} />
         </footer>
       </div>
     )

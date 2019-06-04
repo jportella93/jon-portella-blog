165c7947667acfd8ec10a8070ec06e970f5b2150
jportella93 <jportella93@gmail.com>
2019-06-04 08:08:38 +0200

    style: refactor blog-post to function component

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -6,63 +6,63 @@ import Bio from '../components/Bio'
 import SEO from '../components/seo'
 import { rhythm, scale } from '../utils/typography'
 
-class BlogPostTemplate extends React.Component {
-  render() {
-    const post = this.props.data.markdownRemark
-    const { title: siteTitle, siteUrl } = this.props.data.site.siteMetadata
-    const { previous, next } = this.props.pageContext
-    const { location } = this.props;
-    const postUrl = siteUrl + location.pathname;
+const BlogPostTemplate = ({ data, location, pageContext }) => {
+  const { html: postHtml, frontmatter } = data.markdownRemark;
+  const { title: postTitle, spoiler, date } = frontmatter;
 
-    return (
-      <Layout location={location} title={siteTitle}>
-        <SEO title={post.frontmatter.title} description={post.frontmatter.spoiler} url={postUrl} />
-        <h1>{post.frontmatter.title}</h1>
-        <p
-          style={{
-            ...scale(-1 / 5),
-            display: `block`,
-            marginBottom: rhythm(1),
-            marginTop: rhythm(-1),
-          }}
-        >
-          {post.frontmatter.date}
-        </p>
-        <div dangerouslySetInnerHTML={{ __html: post.html }} />
-        <hr
-          style={{
-            marginBottom: rhythm(1),
-          }}
-        />
-        <Bio />
+  const { title: siteTitle, siteUrl } = data.site.siteMetadata
+  const postUrl = siteUrl + location.pathname;
 
-        <ul
-          style={{
-            display: `flex`,
-            flexWrap: `wrap`,
-            justifyContent: `space-between`,
-            listStyle: `none`,
-            padding: 0,
-          }}
-        >
-          <li>
-            {previous && (
-              <Link to={previous.fields.slug} rel="prev">
-                ← {previous.frontmatter.title}
+  const { previous, next } = pageContext
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <SEO title={postTitle} description={spoiler} url={postUrl} />
+      <h1>{postTitle}</h1>
+      <p
+        style={{
+          ...scale(-1 / 5),
+          display: `block`,
+          marginBottom: rhythm(1),
+          marginTop: rhythm(-1),
+        }}
+      >
+        {date}
+      </p>
+      <div dangerouslySetInnerHTML={{ __html: postHtml }} />
+      <hr
+        style={{
+          marginBottom: rhythm(1),
+        }}
+      />
+      <Bio />
+
+      <ul
+        style={{
+          display: `flex`,
+          flexWrap: `wrap`,
+          justifyContent: `space-between`,
+          listStyle: `none`,
+          padding: 0,
+        }}
+      >
+        <li>
+          {previous && (
+            <Link to={previous.fields.slug} rel="prev">
+              ← {previous.frontmatter.title}
+            </Link>
+          )}
+        </li>
+        <li>
+          {next && (
+            <Link to={next.fields.slug} rel="next">
+              {next.frontmatter.title} →
               </Link>
-            )}
-          </li>
-          <li>
-            {next && (
-              <Link to={next.fields.slug} rel="next">
-                {next.frontmatter.title} →
-              </Link>
-            )}
-          </li>
-        </ul>
-      </Layout>
-    )
-  }
+          )}
+        </li>
+      </ul>
+    </Layout>
+  )
 }
 
 export default BlogPostTemplate

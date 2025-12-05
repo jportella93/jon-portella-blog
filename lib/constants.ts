04b60307e56cce2717ce1cf1f562f38cc2f66188
Jon Portella <jportella93@gmail.com>
2025-12-04 20:35:34 -0500

    Remove .gh-pages-temp submodule for Cloudflare Pages migration

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,9 +1,3 @@
 // Site configuration constants
-export const BASE_PATH = '/jon-portella-blog';
-export const SITE_URL = 'https://jportella93.github.io';
-
-
-
-
-
-
+export const BASE_PATH = "";
+export const SITE_URL = "https://jonportella.com";
